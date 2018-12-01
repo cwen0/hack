68be3a10a380be8a9e3a8633f9625a3654f5d3be
He Cao <hecao@HeCao-MacBook-Pro.local>
2018-12-01 22:10:22 +0800

    implement some operation interfaces

--- ctrl/ctrl.go
+++ ctrl/ctrl.go
@@ -1,75 +1,77 @@
 package main
 
-import (
-	"flag"
-	"time"
+import "github.com/zhouqiang-cl/hack/types"
 
-	"github.com/ngaut/log"
-	"github.com/zhouqiang-cl/hack/types"
-)
-
-var (
-	cmd   string
-	param string
-)
-
-// const
-const (
-	PDADDR  = "127.0.0.1:32800"
-	TIMEOUT = 5 * time.Second
-)
-
-func init() {
-	flag.StringVar(&cmd, "cmd", "failpoint", "which command will use. now support failpoint/partition/record/replay/config/distributary, default is failpoint")
-	flag.StringVar(&param, "param", "", "the param command will use")
-}
-
-// Ctrl is ctrl
-type Ctrl struct {
-	fpCtrl *failpointCtl
-	npCtrl *networkCtl
-	elCtrl *evictLeaderCtl
-	tpCtrl *topology
-}
-
-func newCtrl(toplogic *types.Topological) *Ctrl {
-	return &Ctrl{
-		fpCtrl: newFailpointCtl(toplogic),
-		npCtrl: newNetworkCtl(toplogic),
-		elCtrl: newEvictLeaderCtl(PDADDR, TIMEOUT),
-		tpCtrl: newTopplogyCtl(PDADDR, TIMEOUT),
-	}
-}
-
-func main() {
-	flag.Parse()
-
-	toplogic := getToplogic()
-	ctrl := newCtrl(toplogic)
-	switch cmd {
-	case "failpoint":
-		err := ctrl.fpCtrl.start(param)
-		if err != nil {
-			log.Fatal("failpoint failed %+v", err)
-		}
-	case "network":
-		err := ctrl.npCtrl.start(types.PartitionKind(param))
-		if err != nil {
-			log.Fatalf("network failed %+v", err)
-		}
-	case "evict_leader":
-		err := ctrl.elCtrl.start(param)
-		if err != nil {
-			log.Fatalf("evict leader failed %+v", err)
-		}
-	case "topology":
-		err := ctrl.elCtrl.start(param)
-		if err != nil {
-			log.Fatalf("topology failed %+v", err)
-		}
-	}
-}
-
+//import (
+//	"flag"
+//	"time"
+//
+//	"github.com/ngaut/log"
+//	"github.com/zhouqiang-cl/hack/types"
+//)
+//
+//var (
+//	cmd   string
+//	param string
+//)
+//
+//// const
+//const (
+//	PDADDR  = "127.0.0.1:32800"
+//	TIMEOUT = 5 * time.Second
+//)
+//
+//func init() {
+//	flag.StringVar(&cmd, "cmd", "failpoint", "which command will use. now support failpoint/partition/record/replay/config/distributary, default is failpoint")
+//	flag.StringVar(&param, "param", "", "the param command will use")
+//}
+//
+//// Ctrl is ctrl
+//type Ctrl struct {
+//	fpCtrl *failpointCtl
+//	npCtrl *networkCtl
+//	elCtrl *evictLeaderCtl
+//	tpCtrl *topology
+//}
+//
+//func newCtrl(toplogic *types.Topological) *Ctrl {
+//	return &Ctrl{
+//		fpCtrl: newFailpointCtl(toplogic),
+//		npCtrl: newNetworkCtl(toplogic),
+//		elCtrl: newEvictLeaderCtl(PDADDR, TIMEOUT),
+//		tpCtrl: newTopplogyCtl(PDADDR, TIMEOUT),
+//	}
+//}
+//
+//func main() {
+//	flag.Parse()
+//
+//	toplogic := getToplogic()
+//	ctrl := newCtrl(toplogic)
+//	switch cmd {
+//	case "failpoint":
+//		err := ctrl.fpCtrl.start(param)
+//		if err != nil {
+//			log.Fatal("failpoint failed %+v", err)
+//		}
+//	case "network":
+//		err := ctrl.npCtrl.start(types.PartitionKind(param))
+//		if err != nil {
+//			log.Fatalf("network failed %+v", err)
+//		}
+//	case "evict_leader":
+//		err := ctrl.elCtrl.start(param)
+//		if err != nil {
+//			log.Fatalf("evict leader failed %+v", err)
+//		}
+//	case "topology":
+//		err := ctrl.elCtrl.start(param)
+//		if err != nil {
+//			log.Fatalf("topology failed %+v", err)
+//		}
+//	}
+//}
+//
 func getToplogic() *types.Topological {
 	return &types.Topological{
 		PD:   []string{"10.128.31.5"},
