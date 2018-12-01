d7e46e41d77160276d1da7fc27db4deb6f4efc89
cwen0 <cwenyin0@gmail.com>
2018-12-01 17:01:28 +0800

    make clean

--- dashboard/src/views/Home.vue
+++ dashboard/src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
-    
+    <!--<div :style="{ height: '300px'}" class="clusterChart" ref="clusterChart"></div>-->
+    <div>
+        <h1>xxxxxxxxxxxx</h1>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "home"
+        data() {
+        },
+        created() {
+        },
+        updated() {
+        },
+        methods: {}
     }
 </script>
 
