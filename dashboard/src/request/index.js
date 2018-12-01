e0edc9306edc4998627aefa34e8ceda919c7fd8e
Zhou <zhouqiang@wandoujia.com>
2018-12-01 21:47:11 +0800

    add frontend

--- dashboard/src/request/index.js
+++ dashboard/src/request/index.js
@@ -1,5 +1,10 @@
 // import { REFUSED } from 'dns'
-const Proxy = '/api'
+import axios from 'axios'
+const Proxy = '/api/datasources/proxy/1/api/v1/query_range'
 class Ajax {
+    getDuration(metric, start, end) {
+       var data = axios.get(`${Proxy}?query=histogram_quantile(0.95%2C%20sum(rate(${metric}%5B1m%5D))%20by%20(le))&start=${start}&end=${end}&step=30`)
+        return data
+    }
 }
 export default new Ajax()
